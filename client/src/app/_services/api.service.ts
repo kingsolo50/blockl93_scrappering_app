d5005686a784b63808af8351d6b839162a41e2ac
Solomon Kamanu <skk84@hotmail.com>
2020-06-12 23:11:03 +0100

    package.json edit

--- client/src/app/_services/api.service.ts
+++ client/src/app/_services/api.service.ts
@@ -7,7 +7,7 @@ export class ApiService {
 
   data: any;
 
-  private apiUrl: string = `http://localhost:3000/api/buy-rent-kenya/plots-land-for-sale`;
+  private apiUrl: string = `http://localhost:8000/api/buy-rent-kenya/plots-land-for-sale`;
   private webApi: string = `/api/buy-rent-kenya/plots-land-for-sale`; 
 
   constructor(private http: HttpClient) { }
